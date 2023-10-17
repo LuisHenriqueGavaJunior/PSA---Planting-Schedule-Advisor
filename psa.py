02b29cbc466d846b3c6b3ac9997e77b3a7e338f7
Luis Henrique <euluishenrique7@gmail.com>
2023-10-17 20:34:09 -0300

    Início da produção da parte visual do projeto

--- psa.py
+++ psa.py
@@ -1,76 +1,92 @@
 import requests
-import folium
-from folium.plugins import LocateControl
-# from geolocation.main import GoogleMaps
-# from geolocation.distance_matrix.client import DistanceMatrixApiClient
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nominatim
+from tkinter import ttk, messagebox
+from datetime import *
+import pytz
+from PIL import Image, ImageTk
+from timezonefinder import TimezoneFinder
 
-# Instalar bibliotecas na máquina
-# py -m pip install requests
-# py -m pip install folium
-# py -m pip install geolocation-python
-
-# Criando as variáveis iniciais
-API_KEY = '0673f6221928f8d92ba0dd51c09a1409'
-Latitude = '-22.108567370633455'
-Longitude = '-50.19557700423522'
-Link = f'https://api.openweathermap.org/data/2.5/weather?lat={Latitude}&lon={Longitude}&appid={API_KEY}'
-LinkPrevisao = f'https://api.openweathermap.org/data/2.5/forecast?lat={Latitude}&lon={Longitude}&appid={API_KEY}'
-# google_maps = GoogleMaps(api_key='')
-
-
-# Criando o mapa com a biblioteca Folium
-# Criando uma marcação no mapa com o método Marker a partir da latitude e longitude do usuário
-Mapa = folium.Map([-16.46, -54.70], zoom_start=5)
-LocateControl(auto_start = True, drawMaker = True, markerStyle = dict(color = 'green', weight = 10, opacity = 1, radius = 20)).add_to(Mapa)
-
-# folium.Marker([Latitude, Longitude], tooltip='Você está aqui').add_to(Mapa)
-
-
-# Pegando a previsão do tempo atual
-Requisicao = requests.get(Link)
-Requisicao_dic = Requisicao.json()
-
-# Criando as variáveis do tempo
-Tempo = Requisicao_dic['weather'][0]['description']
-Temperatura = Requisicao_dic['main']['temp']-273
-Umidade = Requisicao_dic['main']['humidity']
-Vento = Requisicao_dic['wind']['speed']
-
-# Traduzindo a variável 'Tempo' para português
-if Tempo == 'clear sky':
-  Tempo = 'Céu limpo'
-
-elif Tempo == 'few clouds':
-   Tempo = 'Com nuvens'
-
-elif Tempo == 'scattered clouds':
-   Tempo = 'Parcialmente nublado'
-
-elif Tempo == 'broken clouds':
-   Tempo = 'Nublado'
-
-elif Tempo == 'shower rain':
-   Tempo = 'Pancadas de chuva'
-
-elif Tempo == 'moderate rain':
-   Tempo = 'Chuva moderada'
-
-elif Tempo == 'rain':
-   Tempo = 'Chovendo'
-
-# Exibindo as informações obtidas pelo WebBot e pelo Folium
-print('Tempo:', Tempo)
-print(f'Temperatura: {Temperatura:.2f}')
-print('Umidade:', Umidade)
-print('Vento:', Vento)
-Mapa
+# Tela base
+root=Tk()
+root.title('PSA')
+root.geometry('890x470+300+300')
+root.configure(bg='#203243')
+root.resizable(True, True)
 
 
 
+def zonatempo():
 
-# print(Requisicao_dic)
+   cidade = textfield.get()
 
-# Pegando a previsão dos próximos 5 dias
-# requisicaoprevisao = requests.get(LinkPrevisao)
-# previsao = requisicaoprevisao.json()
-# print(previsao)
+   geolocator = Nominatim(user_agent='geoapi')
+   localizacao = geolocator.geocode(cidade)
+   obj = Timez
+
+
+   # Criando as variáveis iniciais
+   API_KEY = '0673f6221928f8d92ba0dd51c09a1409'
+   Latitude = '-22.108567370633455'
+   Longitude = '-50.19557700423522'
+   Link = f'https://api.openweathermap.org/data/2.5/weather?lat={Latitude}&lon={Longitude}&appid={API_KEY}'
+   LinkPrevisao = f'https://api.openweathermap.org/data/2.5/forecast?lat={Latitude}&lon={Longitude}&appid={API_KEY}'
+   
+   # Pegando a previsão do tempo atual
+   Requisicao = requests.get(Link)
+   Requisicao_dic = Requisicao.json()
+
+   # Criando as variáveis do tempo
+   Tempo = Requisicao_dic['weather'][0]['description']
+   Temperatura = Requisicao_dic['main']['temp']-273
+   Umidade = Requisicao_dic['main']['humidity']
+
+   tempolab.config(text=(Tempo))
+   temperaturalab.config(text=(Temperatura, '°C'))
+   umidadelab.config(text=(Umidade, '%'))
+
+   # Traduzindo a variável 'Tempo' para português
+   if Tempo == 'clear sky':
+      Tempo = 'Céu limpo'
+   elif Tempo == 'few clouds':
+      Tempo = 'Com nuvens'
+   elif Tempo == 'scattered clouds':
+      Tempo = 'Parcialmente nublado'
+   elif Tempo == 'overcast clouds':
+      Tempo = 'Nublado'
+   elif Tempo == 'shower rain':
+      Tempo = 'Pancadas de chuva'
+   elif Tempo == 'moderate rain':
+      Tempo = 'Chuva moderada'
+   elif Tempo == 'rain':
+      Tempo = 'Chovendo'
+
+
+
+label1=Label(root, text='Tempo', font=('Helvetica', 11), fg='white', bg='#203243')
+label1.place(x=50, y=120)
+
+label2=Label(root, text='Temperatura', font=('Helvetica', 11), fg='white', bg='#203243')
+label2.place(x=50, y=140)
+
+label3=Label(root, text='Umidade', font=('Helvetica', 11), fg='white', bg='#203243')
+label3.place(x=50, y=160)
+
+tempolab = Label(root, font=('Helvetica', 11), fg='white', bg='#203243')
+tempolab.place(x=170, y=120)
+
+temperaturalab = Label(root, font=('Helvetica', 11), fg='white', bg='#203243')
+temperaturalab.place(x=170, y=140)
+
+umidadelab = Label(root, font=('Helvetica', 11), fg='white', bg='#203243')
+umidadelab.place(x=170, y=160)
+
+
+
+
+
+
+# Iniciando tela
+root.mainloop()
+
